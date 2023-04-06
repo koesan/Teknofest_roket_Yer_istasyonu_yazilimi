daec1af1fc4c9440237c9e49dd644218dc5cee41
Barış KÜMET <96130124+koesan@users.noreply.github.com>
2023-04-06 11:10:23 +0300

    Update ui_to_py.py

--- ui_to_py.py
+++ ui_to_py.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Mar 13 11:58:46 2019
 
-@author: ali
-"""
 #designerden .ui tasarımını değiştirdiğinde çalıştırılır.
 #çalıştırmadan değiştirdiğiniz dosya ya göre, open ve uic.compileUi() deki isimleri değiştirdiğiniz.
 
