437aaeb76ff413e89bb19d7e80dc4b45a757acf8
Barış KÜMET <96130124+koesan@users.noreply.github.com>
2023-04-06 11:08:30 +0300

    Update ui_to_py.py

--- ui_to_py.py
+++ ui_to_py.py
@@ -4,10 +4,10 @@ Created on Wed Mar 13 11:58:46 2019
 
 @author: ali
 """
-#designerden .ui tasarımını değiştirdiğinde çalıştırılır
+#designerden .ui tasarımını değiştirdiğinde çalıştırılır.
+#çalıştırmadan değiştirdiğiniz dosya ya göre, open ve uic.compileUi() deki isimleri değiştirdiğiniz.
 
 from PyQt5 import uic
 
-
-with open('yeristasyonu.py', 'w', encoding="utf-8") as fout:
-  uic.compileUi('yeristasyonu.ui', fout)
+with open("dosya'nın adı", 'w', encoding="utf-8") as fout:
+  uic.compileUi("dosya'nın adı", fout)
